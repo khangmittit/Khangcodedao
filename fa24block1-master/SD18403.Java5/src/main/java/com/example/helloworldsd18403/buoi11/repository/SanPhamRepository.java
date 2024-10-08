f11e7aa4a0caedcf939832a315b1fd5fcd25db0f
Khang Nguyễn <141286031+khangmittit@users.noreply.github.com>
2024-10-08 18:07:36 +0700

    Revert "tt"

--- fa24block1-master/SD18403.Java5/src/main/java/com/example/helloworldsd18403/buoi11/repository/SanPhamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.helloworldsd18403.buoi11.repository;
-
-import com.example.helloworldsd18403.buoi11.entity.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
-}
--- fa24block1-master/SD19202.Java5/src/main/java/com/example/helloworldsd19202/buoi11/repository/SanPhamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.helloworldsd19202.buoi11.repository;
-
-import com.example.helloworldsd19202.buoi11.entity.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
-}
